8d71dec96aa45bb28444363199b345e045810885
Ivan Buldakov <pereslav88@gmail.com>
2022-09-11 18:54:40 +0300

    Merge_sort modified

--- Sort/Task1/merge_sort.cpp
+++ Sort/Task1/merge_sort.cpp
@@ -2,58 +2,66 @@
 #include "windows.h"
 #include <iostream>
 
-int* merge(int* A, int size_A, int* B, int size_B) {
+void merge(int* arr1, int size_arr1, int* arr2, int size_arr2, int* final_arr) {
     
-    
-    int* final_arr = new int[size_A + size_B];
+    int arr1_index = 0;
+    int arr2_index = 0;
+    int final_arr_index = 0;
 
-    int ia = 0, ib = 0, ic = 0;
-    while (ia < size_A || ib < size_B) {
-        if (ia == size_A)
-            final_arr[ic++] = B[ib++];           
-        else if (ib == size_B)
-            final_arr[ic++] = A[ia++];             
-                                            
-        else if (A[ia] <= B[ib])              
-            final_arr[ic++] = A[ia++];            
-        else                                 
-                                               
-                                              
-            final_arr[ic++] = B[ib++];                                     
-    }                                                              
-                                                                     
+    while (arr1_index < size_arr1 && arr2_index < size_arr2) {
+        if (arr1[arr1_index] <= arr2[arr2_index]) {
+            final_arr[final_arr_index] = arr1[arr1_index];
+            arr1_index++;
+        }
+        else {
+            final_arr[final_arr_index] = arr2[arr2_index];
+            arr2_index++;
+        }
+        final_arr_index++;
+    }
+
+    while (arr1_index < size_arr1) {
+        final_arr[final_arr_index] = arr1[arr1_index];
+        arr1_index++;
+        final_arr_index++;
+    }
+
+    while (arr2_index < size_arr2) {
+        final_arr[final_arr_index] = arr2[arr2_index];
+        arr2_index++;
+        final_arr_index++;
+    }
+
     
-    return final_arr;
 }                                                                   
-int* merge_sort(int* arr, int size) {                      
+void merge_sort(int* arr, int size) {                      
     
-    if (size == 1) return arr;
+    if (size < 2) return;
     
     int mid = size / 2;
     
    
-    int left_size = mid + 1;
+    int left_size = mid;
     int right_size = size - mid;
     int* left = new int[left_size];
     int* right = new int[right_size];
-    int* left_sorted;
-    int* right_sorted;
-    for (int i = 0; i < mid + 1; i++) {
+   
+    for (int i = 0; i < mid; i++) {
         left[i] = arr[i];
     }
 
-    for (int i = 0, k = mid + 1; i < mid + 1, k < size + 1; i++, k++) {
-        right[i] = arr[k];
+    for (int i = mid; i < size; i++) {
+        right[i-mid] = arr[i];
     }
 
-    left_sorted = merge_sort(left, left_size-1);
-    right_sorted = merge_sort(right, right_size-1);
-    return merge(left_sorted, left_size, right_sorted, right_size);
+    merge_sort(left, left_size);
+    merge_sort(right, right_size);
+    merge(left, left_size, right, right_size, arr);
+   
 
     delete[] left;
     delete[] right;
-    delete[] left_sorted;
-    delete[] right_sorted;
+  
 
 }
 
@@ -70,120 +78,13 @@ int main()
     SetConsoleOutputCP(1251);
     int arr1[10] = {3, 43, 38, 29, 18, 72, 57, 61, 2, 33};
     int size = sizeof(arr1) / sizeof(arr1[0]);
+    std::cout << "Исходный массив: ";
     arr_display(arr1, size);
-    int* final_arr = new int[size];
-    final_arr = merge_sort(arr1, size-1);
-    arr_display(final_arr, size);
-    delete[] final_arr;
+    merge_sort(arr1, size);
+    std::cout << "Отсортированный массив: ";
+    arr_display(arr1, size);
+   
     return 0;
 }
 
 
-/*
-// C++ program for Merge Sort
-#include <iostream>
-using namespace std;
-
-// Merges two subarrays of array[].
-// First subarray is arr[begin..mid]
-// Second subarray is arr[mid+1..end]
-void merge(int array[], int const left, int const mid,
-    int const right)
-{
-    auto const subArrayOne = mid - left + 1;
-    auto const subArrayTwo = right - mid;
-
-    // Create temp arrays
-    auto* leftArray = new int[subArrayOne],
-        * rightArray = new int[subArrayTwo];
-
-    // Copy data to temp arrays leftArray[] and rightArray[]
-    for (auto i = 0; i < subArrayOne; i++)
-        leftArray[i] = array[left + i];
-    for (auto j = 0; j < subArrayTwo; j++)
-        rightArray[j] = array[mid + 1 + j];
-
-    auto indexOfSubArrayOne
-        = 0, // Initial index of first sub-array
-        indexOfSubArrayTwo
-        = 0; // Initial index of second sub-array
-    int indexOfMergedArray
-        = left; // Initial index of merged array
-
-    // Merge the temp arrays back into array[left..right]
-    while (indexOfSubArrayOne < subArrayOne
-        && indexOfSubArrayTwo < subArrayTwo) {
-        if (leftArray[indexOfSubArrayOne]
-            <= rightArray[indexOfSubArrayTwo]) {
-            array[indexOfMergedArray]
-                = leftArray[indexOfSubArrayOne];
-            indexOfSubArrayOne++;
-        }
-        else {
-            array[indexOfMergedArray]
-                = rightArray[indexOfSubArrayTwo];
-            indexOfSubArrayTwo++;
-        }
-        indexOfMergedArray++;
-    }
-    // Copy the remaining elements of
-    // left[], if there are any
-    while (indexOfSubArrayOne < subArrayOne) {
-        array[indexOfMergedArray]
-            = leftArray[indexOfSubArrayOne];
-        indexOfSubArrayOne++;
-        indexOfMergedArray++;
-    }
-    // Copy the remaining elements of
-    // right[], if there are any
-    while (indexOfSubArrayTwo < subArrayTwo) {
-        array[indexOfMergedArray]
-            = rightArray[indexOfSubArrayTwo];
-        indexOfSubArrayTwo++;
-        indexOfMergedArray++;
-    }
-    delete[] leftArray;
-    delete[] rightArray;
-}
-
-// begin is for left index and end is
-// right index of the sub-array
-// of arr to be sorted
-void mergeSort(int array[], int const begin, int const end)
-{
-    if (begin >= end)
-        return; // Returns recursively
-
-    auto mid = begin + (end - begin) / 2;
-    mergeSort(array, begin, mid);
-    mergeSort(array, mid + 1, end);
-    merge(array, begin, mid, end);
-}
-
-// UTILITY FUNCTIONS
-// Function to print an array
-void printArray(int A[], int size)
-{
-    for (auto i = 0; i < size; i++)
-        cout << A[i] << " ";
-}
-
-// Driver code
-int main()
-{
-    int arr[] = { 12, 11, 13, 5, 6, 7 };
-    auto arr_size = sizeof(arr) / sizeof(arr[0]);
-
-    cout << "Given array is \n";
-    printArray(arr, arr_size);
-
-    mergeSort(arr, 0, arr_size - 1);
-
-    cout << "\nSorted array is \n";
-    printArray(arr, arr_size);
-    return 0;
-}
-
-// This code is contributed by Mayank Tyagi
-// This code was revised by Joshua Estes
-*/
